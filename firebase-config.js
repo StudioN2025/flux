b8f1bed1dac2ea1f6162bc8979f898f86a290df9
StudioN2025 <abobachebaksar@gmail.com>
2026-03-20 11:06:29 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-// Конфигурация Firebase для проекта flux-messenger-bbf58
 const firebaseConfig = {
-    apiKey: "AIzaSyBMHBR2bLJYCq16gK-e7QaxN0ummuN_ZIo",
-    authDomain: "flux-messenger-bbf58.firebaseapp.com",
-    projectId: "flux-messenger-bbf58",
-    storageBucket: "flux-messenger-bbf58.firebasestorage.app",
-    messagingSenderId: "ТВОЙ_ID",
-    appId: "ТВОЙ_APP_ID"
+  apiKey: "AIzaSyBMHBR2bLJYCq16gK-e7QaxN0ummuN_ZIo",
+  authDomain: "flux-messenger-bbf58.firebaseapp.com",
+  projectId: "flux-messenger-bbf58",
+  storageBucket: "flux-messenger-bbf58.firebasestorage.app",
+  messagingSenderId: "143785700355",
+  appId: "1:143785700355:web:17c1c94e1faf70bfe87236"
 };
 
 // Инициализация Firebase
